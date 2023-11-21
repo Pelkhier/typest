59f64345c6f42bee87a684d15b2915320db7c7da
mohammed pelkhier <pelkhier96@gmail.com>
2023-11-21 15:34:53 +0200

    add: vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 import { dev } from "$app/environment";
 import { inject } from "@vercel/analytics";
 
+// This function for initializing Vercel analytics tools
 inject({ mode: dev ? "development" : "production" });
